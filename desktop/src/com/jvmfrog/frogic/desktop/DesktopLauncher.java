701fb5b5b561ba7dfa7dd2bdde3fd82f9a534c7c
Kirill <kirbo06@mail.ru>
2022-09-23 21:48:38 +0200

    init

---
+++ desktop/src/com/jvmfrog/frogic/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jvmfrog.frogic.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jvmfrog.frogic.Main;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Main(), config);
+	}
+}
